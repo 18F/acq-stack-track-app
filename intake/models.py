db5bbfa3ebd6ec7403b9d1aa7c932cd5f128984d
Alan deLevie <adelevie@gmail.com>
2017-05-02 15:47:35 -0400

    Added Request model

--- intake/models.py
+++ intake/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Request(models.Model):
+    below_mp_threshold = models.NullBooleanField()
+    is_training = models.NullBooleanField()
+    is_internal = models.NullBooleanField()
+    client_has_approval = models.NullBooleanField()
+    client_contact = models.CharField(max_length=200)
+    urgency = models.TextField()
+    description = models.TextField()
+    submitted_at = models.DateTimeField(null=True)
