125723bc6516d6680ba9b7523d49ad32b06a5627
Alan deLevie <adelevie@gmail.com>
2017-05-03 13:26:17 -0400

    Fleshing out CreateRequest unit test

--- intake/tests.py
+++ intake/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from intake.models import Request
+from intake.services import CreateRequest
+
+class CreateRequestTestCase(TestCase):
+    def test_perform_with_no_args(self):
+        create_request = CreateRequest()
+        self.assertEqual(Request.objects.count(), 0)
+        request = create_request.perform()
+        self.assertEqual(Request.objects.count(), 1)
+        self.assertEqual(type(request.pk), int)
