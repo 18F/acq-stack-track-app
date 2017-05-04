2b9ba973af077f2bda220c2c73fc0692b480946b
steven.reilly@gsa.gov <steven.reilly@gsa.gov>
2017-05-04 10:39:21 -0500

    add us web design standards files

--- tracker/settings.py
+++ tracker/settings.py
@@ -167,4 +167,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "third-party/uswds-1.0.0")
+]
